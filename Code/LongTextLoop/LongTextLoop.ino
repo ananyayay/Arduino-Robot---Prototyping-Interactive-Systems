b39d212321705294f4474b72d971c45b2bb8de91
Aflah <72096386+aflah02@users.noreply.github.com>
2022-05-15 00:05:55 +0530

    LongLoop

--- Code/LongTextLoop/LongTextLoop.ino
+++ Code/LongTextLoop/LongTextLoop.ino
@@ -1,16 +1,19 @@
 #include <LiquidCrystal.h>
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);          // select the pins used on the LCD panel
+String myStrings[] = {"1. 8th March",  "2. 21st April", "3. 25th December", "4. 1st January"};
 
-void setup(){
-    lcd.begin(16, 2);                                 // set up a 2x16 character display
-    lcd.print("Hello World!");                       // print a message to the LCD
-    lcd.setCursor(0, 1);                             // set the cursor to column 0, line 1
-    lcd.print("Hello World!");                       // print a message to the LCD
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Women's Day");
 }
 
-void loop(){
-  // Scroll Text
-    lcd.scrollDisplayLeft();                           // scroll the text one space to the left
-    delay(500);                                       // wait 500ms
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(myStrings[(millis() / 1000)%4]);
 }
