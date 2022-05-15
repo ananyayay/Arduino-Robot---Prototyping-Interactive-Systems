3fce6726d7d8347919b7fd2c161362c0aebb958a
Aflah <72096386+aflah02@users.noreply.github.com>
2022-05-15 19:43:25 +0530

    So Far

--- Code/sketch_may15a/sketch_may15a.ino
+++ Code/sketch_may15a/sketch_may15a.ino
@@ -1,3 +1,5 @@
+
+//www.diyusthad.com
 #include <LiquidCrystal.h>
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
--- Code/sofar.ino
+++ Code/sofar.ino
@@ -1,7 +1,17 @@
-//Michael Klements
-//The DIY Life
-//17 May 2019 
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int melody[]={262, 196, 196, 220, 196, 0, 247, 262};
+String Questions[4] = {"1. What is the capital of India?", 
+                      "2. What is the capital of France?", 
+                      "3. What is the capital of Germany?", 
+                      "4. What is the capital of Italy?"};
+String options[4] = {"1. Delhi 2. Paris 3. Berlin 4. Rome",
+                        "1. Paris 2. London 3. Berlin 4. Rome",
+                        "1. London 2. Berlin 3. Paris 4. Rome",
+                        "1. Paris 2. London 3. Rome 4. Berlin"};
+String ans[4] = {"A", "A", "B", "C"};
 int noteDurations[]={4, 8, 8, 4, 4, 4, 4, 4};
 int ledPin = LED_BUILTIN;    // choose the pin for the LED
 int input5Pin = 6;       // define push button input pins
@@ -9,6 +19,10 @@ int input4Pin = 7;
 int input3Pin = 8;
 int input2Pin = 9;
 int buzzerPin = 10;
+int score;
+int Lost;
+int optionNum;
+int quesNum;
 void setup()
 {
   pinMode(ledPin, OUTPUT);   // declare LED as output
@@ -17,10 +31,19 @@ void setup()
   pinMode(input3Pin, INPUT);
   pinMode(input2Pin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+    Lost = 0;
+    optionNum = 0;
+    score = 0;
+    quesNum = 0;
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // Print a message to the LCD.
+    lcd.print(Questions[quesNum]);
 }
 
 void loop()
 {
+  optionNum++;
 //  digitalWrite(ledPin, LOW);
   for(int i=6 ; i<=9 ; i++)
     checkPush (i);
@@ -28,6 +51,10 @@ void loop()
 
 void checkPush(int pinNumber)
 {
+  lcd.scrollDisplayLeft();
+  lcd.setCursor(0, 1);  
+  lcd.print(options[quesNum]);  
+  delay(400);
   int pushed = digitalRead(pinNumber);  // read input value
   if (pushed == HIGH) {// check if the input is HIGH (button released)
     digitalWrite(ledPin, LOW);  // turn LED OFF
