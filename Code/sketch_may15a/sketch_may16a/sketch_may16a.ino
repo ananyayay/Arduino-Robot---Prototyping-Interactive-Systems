3a03a30ca7141a792a9acef4d7f5f3a11bd12caa
Aflah <72096386+aflah02@users.noreply.github.com>
2022-05-16 19:58:24 +0530

    Thanks

--- Code/sketch_may15a/sketch_may16a/sketch_may16a.ino
+++ Code/sketch_may15a/sketch_may16a/sketch_may16a.ino
@@ -1,66 +1,24 @@
-#include <IRremote.h> // include the IRremote library
-int IR_RECEIVE_PIN = 7;
-long data = 0; 
-int noteDurationsWin[]={4, 8, 8, 4, 4, 4, 4, 4};
-int melodyWin[]={262, 196, 196, 220, 196, 0, 247, 262};
-int buzzerPin = 6;
-void setup() {
-  Serial.begin(9600); // begin serial communication with a baud rate of 9600
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  pinMode(buzzerPin, OUTPUT);
+/*      
+*  IR read codes     
+*  by Hanie kiani     
+*  https://electropeak.com/learn/        
+*/     
+#include <IRremote.h>  //including infrared remote header file     
+int RECV_PIN = 13; // the pin where you connect the output pin of IR sensor     
+IRrecv irrecv(RECV_PIN);     
+decode_results results;     
+void setup()     
+{     
+Serial.begin(9600);     
+irrecv.enableIRIn();     
 }
- void PlayWin(){
-  for (int thisNote=0; thisNote <8; thisNote++){
-
-    //to calculate the note duration, take one second. Divided by the note type
-    int noteDuration = 1000 / noteDurationsWin [thisNote];
-    tone(buzzerPin, melodyWin[thisNote], noteDuration);
-
-    //to distinguish the notes, set a minimum time between them
-    //the note's duration +30% seems to work well
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-
-    //stop the tone playing
-    noTone(8);
-  }
-} 
-void loop() {
-  if (IrReceiver.decode()) {
-    Serial.println("HELO");
-    if (IrReceiver.decodedIRData.decodedRawData != 0) {
-      data = IrReceiver.decodedIRData.decodedRawData;
-      Serial.println(data);
-    }
- 
-    if (data == -217252096){
-        Serial.println("1, Red");
-        PlayWin();
-    }
-    else if (data == -417792256){
-        Serial.println("2, Orange");
-        PlayWin();
-    }
-    else if (data == -1587609856){
-        Serial.println("3, Yellow");
-        PlayWin();
-    }
-    else if (data == -150405376){
-        Serial.println("4, Green");
-        PlayWin();
-    }
-    else if (data == -484638976){
-        Serial.println("5, Blue");
-        PlayWin();
-    }
-    else if (data == -1520763136){
-        Serial.println("6, Intigo");
-        PlayWin();
-    }
-    else if (data == -1119682816){
-        Serial.println("7, Violet");
-        PlayWin();
-    }
-    }
-    IrReceiver.resume();
-  }
+void loop() {     
+//  Serial.println("HEre");
+if (irrecv.decode(&results)){     
+ Serial.println(" ");     
+ Serial.print("Code: ");     
+ Serial.println(results.value, HEX); //prints the value a a button press     
+ Serial.println(" ");     
+ irrecv.resume(); // Restart the ISR state machine and Receive the next value     
+}   
+}
--- Code/sketch_may15a/sketch_may16a/sketch_may16a/sketch_may16a.ino
+++ Code/sketch_may15a/sketch_may16a/sketch_may16a/sketch_may16a.ino
@@ -1,5 +1,5 @@
 #include <LiquidCrystal.h>
-#include <IRremote.h>
+
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int melodyWin[]={262, 196, 196, 220, 196, 0, 247, 262};
